6e8c76191c03bc5e4e487969a7bbc40d751ca2fc
azcao95 <azcao95@gmail.com>
2025-06-12 02:35:36 -0500

    pep8 styling

--- overthecap/overthecap/settings.py
+++ overthecap/overthecap/settings.py
@@ -14,65 +14,65 @@ NEWSPIDER_MODULE = 'overthecap.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'overthecap (+http://www.yourdomain.com)'
+# USER_AGENT = 'overthecap (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+# CONCURRENT_REQUESTS = 32
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+# DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 16
+# CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+# COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
+# TELNETCONSOLE_ENABLED = False
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
-  'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-  'Accept-Language': 'en',
-  'User-Agent': "Mozilla/5.0"
-}
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en',
+    'User-Agent': "Mozilla/5.0"}
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
+# SPIDER_MIDDLEWARES = {
 #    'overthecap.middlewares.OverthecapSpiderMiddleware': 543,
-#}
+# }
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
+# DOWNLOADER_MIDDLEWARES = {
 #    'overthecap.middlewares.OverthecapDownloaderMiddleware': 543,
-#}
+# }
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
+# EXTENSIONS = {
 #    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
+# }
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
+# ITEM_PIPELINES = {
 #    'overthecap.pipelines.OverthecapPipeline': 300,
-#}
+# }
 
-# Increase concurrency for faster crawling, but be mindful of target site limits.
+# Increase concurrency for faster crawling, but be mindful of target site
+# limits.
 CONCURRENT_REQUESTS = 32
 CONCURRENT_REQUESTS_PER_DOMAIN = 16
 CONCURRENT_REQUESTS_PER_IP = 16
 DOWNLOAD_DELAY = 0.05
-RANDOMIZE_DOWNLOAD_DELAY=True
+RANDOMIZE_DOWNLOAD_DELAY = True
 AUTOTHROTTLE_ENABLED = True
 # The initial download delay
 AUTOTHROTTLE_START_DELAY = .5
@@ -86,8 +86,8 @@ AUTOTHROTTLE_DEBUG = True
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# HTTPCACHE_ENABLED = True
+# HTTPCACHE_EXPIRATION_SECS = 0
+# HTTPCACHE_DIR = 'httpcache'
+# HTTPCACHE_IGNORE_HTTP_CODES = []
+# HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
--- overthecap/overthecap/spiders/otc_spider.py
+++ overthecap/overthecap/spiders/otc_spider.py
@@ -1,6 +1,7 @@
 import scrapy
 import re
 
+
 class OverthecapSpider(scrapy.Spider):
     name = "overthecap"
     allowed_domains = ["overthecap.com"]
@@ -27,22 +28,25 @@ class OverthecapSpider(scrapy.Spider):
     def parse(self, response):
         for url in self.start_urls:
             yield scrapy.Request(url, callback=self.parse_contracts)
-    
-    def parse_contract_details(self, response):
-        print(f"Parsing contract details for {response.meta.get('player_name', 'Unknown')}")
 
-        pay_table = response.xpath('//div[@class="contract-container"]//table[@class="contract salary-cap-history player-new"]')
+    def parse_contract_details(self, response):
+        print(
+            f"Parsing contract details for {response.meta.get('player_name', 'Unknown')}")
+
+        pay_table = response.xpath(
+            '//div[@class="contract-container"]//table[@class="contract salary-cap-history player-new"]')
         if not pay_table:
             print("No pay table found in the response.")
             return
-        
+
         headers = pay_table.xpath('.//tr[1]/th').getall()
         if not headers:
             print("No headers found in the pay table.")
             return
 
         # XPath to select <tr> elements where a <td> has text exactly '2024'
-        target_row = pay_table.xpath('.//tr[td[normalize-space(text())="2024"]]')
+        target_row = pay_table.xpath(
+            './/tr[td[normalize-space(text())="2024"]]')
         if not target_row:
             print("No target row found for the year 2024.")
             return
@@ -51,12 +55,26 @@ class OverthecapSpider(scrapy.Spider):
         if not target_row_cells:
             print("No cells found in the target row.")
             return
-        
+
         result = {}
 
         for header, cell in zip(headers, target_row_cells):
-            header_text = re.sub('<[^<]+?>', '', header).strip().lower().replace(' ', '_').replace('%', 'percent')
-            cell_text = re.sub('<[^<]+?>', '', cell).strip().replace('$', '').replace(',', '')
+            header_text = re.sub(
+                '<[^<]+?>',
+                '',
+                header).strip().lower().replace(
+                ' ',
+                '_').replace(
+                '%',
+                'percent')
+            cell_text = re.sub(
+                '<[^<]+?>',
+                '',
+                cell).strip().replace(
+                '$',
+                '').replace(
+                ',',
+                '')
             result[header_text] = cell_text
 
         yield {
@@ -68,7 +86,7 @@ class OverthecapSpider(scrapy.Spider):
         # Extract the years active from the string
         if not years_active_string:
             return None
-        
+
         # Replace em dash with regular hyphen and split
         years_active_arr = years_active_string.replace('‑', '-').split('-')
         print("ARRAY:", years_active_arr)
@@ -82,7 +100,8 @@ class OverthecapSpider(scrapy.Spider):
         elif len(years_active_arr) == 2:
             start_year = int(years_active_arr[0].strip())
             # Handle empty end_year (ongoing career)
-            end_year = int(years_active_arr[1].strip()) if years_active_arr[1].strip() else 2024
+            end_year = int(years_active_arr[1].strip(
+            )) if years_active_arr[1].strip() else 2024
             if start_year <= 2024 and end_year >= 2024:
                 return True
             else:
@@ -90,20 +109,20 @@ class OverthecapSpider(scrapy.Spider):
         else:
             print(f"Unexpected format for years active: {years_active_string}")
             return None
-       
 
     def parse_contracts(self, response):
-        earnings_table = response.xpath('//table[@class="career-earnings-position position-table sortable"]')
+        earnings_table = response.xpath(
+            '//table[@class="career-earnings-position position-table sortable"]')
         if not earnings_table:
             print("No earnings table found in the response.")
             return
         # Extract column names from the table header
-        
+
         rows = earnings_table.xpath('.//tbody/tr')
         if not rows:
             print("No rows found in the earnings table.")
             return
-        
+
         for row in rows:
             # Skip the header row
             if row.xpath('.//th'):
@@ -112,8 +131,9 @@ class OverthecapSpider(scrapy.Spider):
             link = row.xpath('.//td[1]//a/@href').get()
             if link:
                 # Construct the full URL for the contract details
-                full_url = response.urljoin(link)+'#contract-history'
-                player_name = row.xpath('.//td[1]//a/text()').get().strip().replace(',', '')
+                full_url = response.urljoin(link) + '#contract-history'
+                player_name = row.xpath(
+                    './/td[1]//a/text()').get().strip().replace(',', '')
 
                 if not player_name:
                     print("Player name not found in the row.")
@@ -126,10 +146,11 @@ class OverthecapSpider(scrapy.Spider):
 
                 is_active_2024 = self.parse_active_2024(years_active_string)
 
-                if is_active_2024 == True:
+                if is_active_2024:
                     yield scrapy.Request(full_url, callback=self.parse_contract_details, meta={'player_name': player_name})
                 else:
-                    print(f"Player {player_name} is not active in 2024, skipping contract details.")
+                    print(
+                        f"Player {player_name} is not active in 2024, skipping contract details.")
                     continue
             else:
                 continue
